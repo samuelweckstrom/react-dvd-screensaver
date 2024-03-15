16f4ce708774830e1c1244d395fee172879c844f
Samuel Weckstrom <samuel.weckstrom@gmail.com>
2024-03-15 19:24:42 +0100

    feat: update tooling

---
+++ demo/src/main.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+
+console.log('main.tsx');
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <App />
+);
