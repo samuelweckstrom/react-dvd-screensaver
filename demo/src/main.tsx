2eda3067dac2c077823fb69d9cc68eb8aa436158
Samuel Weckström <samuel.weckstrom@gmail.com>
2026-04-16 08:38:46 +0200

    [refactor]: Hook rewrite (#43)
    * refactor: new version
    
    * fix: update module resolution

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
-console.log('main.tsx');
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />
 );
